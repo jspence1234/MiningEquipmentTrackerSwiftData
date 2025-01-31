2d497723697f189ec47d340f6ae45f8c5c95ec5d
jacobspence <jacobspence@MacBook-Air.local>
2025-01-31 08:15:15 -0600

    Initial Commit

---
+++ MiningEquipmentTrackerSwiftDataTests/MiningEquipmentTrackerSwiftDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiningEquipmentTrackerSwiftDataTests.swift
+//  MiningEquipmentTrackerSwiftDataTests
+//
+//  Created by Jacob Spence on 1/31/25.
+//
+
+import Testing
+@testable import MiningEquipmentTrackerSwiftData
+
+struct MiningEquipmentTrackerSwiftDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
